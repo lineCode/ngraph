d708d875b0eea40881ed2ded732a910b836b715d
Fenglei Tian <fengleit@fengleit-mac01.fso.intel.com>
2018-03-15 10:11:51 -0700

    still need cublas_v2.h

--- src/ngraph/runtime/gpu/gpu_emitter.cpp
+++ src/ngraph/runtime/gpu/gpu_emitter.cpp
@@ -16,7 +16,7 @@
 
 #include <algorithm>
 #include <cmath>
-#include <cublas.h>
+#include <cublas_v2.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cudnn.h>
--- src/ngraph/runtime/gpu/gpu_util.hpp
+++ src/ngraph/runtime/gpu/gpu_util.hpp
@@ -21,7 +21,7 @@
 #include <string>
 #include <vector>
 
-#include <cublas.h>
+#include <cublas_v2.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cudnn.h>
